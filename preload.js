31346f665fab021606796d7ebe1a2e0962b7f8aa
Yasuhiro Yamaguchi <yamaguchi.yshr@gmail.com>
2026-09-06 04:03:14 +0900

    Update preload.js

--- preload.js
+++ preload.js
@@ -8,5 +8,8 @@ contextBridge.exposeInMainWorld('electronAPI', {
   previewMonitor: (index) => ipcRenderer.send('preview-monitor', index),
 
   // 決定したモニターを main.js に送る
-  selectMonitor: (index) => ipcRenderer.send('monitor-selected', index)
+  selectMonitor: (index) => ipcRenderer.send('monitor-selected', index),
+
+  // ★ WebSocket で受信したコメントを main.js に送る（CSV 保存用）
+  saveComment: (data) => ipcRenderer.send('comment-received', data)
 });
