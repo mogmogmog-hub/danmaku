2ef11bb7580dcab57dbf60677f0e115f93d228f2
Yasuhiro Yamaguchi <yamaguchi.yshr@gmail.com>
2026-09-06 04:50:59 +0900

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,15 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  // モニター一覧を main.js に問い合わせる
   getDisplays: () => ipcRenderer.invoke('get-displays'),
-
-  // 仮選択（選択中オーバーレイ表示）
   previewMonitor: (index) => ipcRenderer.send('preview-monitor', index),
-
-  // 決定したモニターを main.js に送る
   selectMonitor: (index) => ipcRenderer.send('monitor-selected', index),
 
-  // ★ WebSocket で受信したコメントを main.js に送る（CSV 保存用）
-  saveComment: (data) => ipcRenderer.send('comment-received', data)
+  saveComment: (data) => ipcRenderer.send('comment-received', data),
+
+  // ★ コメント履歴読み込み
+  loadHistory: () => ipcRenderer.invoke('load-history')
 });
